1dbd9faf115f98ad442616a7303c91073a8c2aed
jngrt <jangeertmunneke@gmail.com>
2012-10-17 12:33:47 +0200

    changed data source

--- web.rb
+++ web.rb
@@ -4,10 +4,11 @@ require 'net/http'
 
 get '/' do
   xcoord = "4.60000000000005" + rand(1000).to_s
-  url = "http://marinetraffic.com/ais/getjson.aspx?"+
-        "sw_x=3.8&sw_y=51.8&ne_x="+xcoord+
-        "&ne_y=52&"+
-        "zoom=12&fleet=&station=0"
+  #url = "http://marinetraffic.com/ais/getjson.aspx?"+
+  #      "sw_x=3.8&sw_y=51.8&ne_x="+xcoord+
+  #      "&ne_y=52&"+
+  #      "zoom=12&fleet=&station=0"
+  url = "http://marinetraffic.com/ais/getjson.aspx?sw_x=3&sw_y=51&ne_x=5&ne_y=53&zoom=10&fleet=&station=0"
   resp = Net::HTTP.get_response(URI.parse(url))
   raw = resp.body
   #clean = raw.sub(",,",",0,")
