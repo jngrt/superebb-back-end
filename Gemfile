bf9db1a2435096c2b8064518314703b68a91a70b
jngrt <jangeertmunneke@gmail.com>
2012-10-17 17:29:57 +0200

    gems updated

--- Gemfile
+++ Gemfile
@@ -1,3 +1,11 @@
 source :rubygems
 gem 'sinatra', '1.1.0'
 gem 'thin'
+gem 'data_mapper'
+
+group :development, :test do
+ gem 'dm-sqlite-adapter'
+end
+group :production do
+  gem 'dm-postgres-adapter'
+end
--- web.rb
+++ web.rb
@@ -1,17 +1,44 @@
 require 'sinatra'
 require 'json'
 require 'net/http'
+require 'data_mapper'
+
+#DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://test.sqlite')
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] ||
+                 "sqlite3://#{Dir.pwd}/test.db")
+
+class ShipData
+  include DataMapper::Resource
+  property :id, Serial
+  property :json, Text
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+DataMapper.auto_upgrade!
 
 get '/' do
-  xcoord = "4.60000000000005" + rand(1000).to_s
+  if ShipData.count == 0
+    puts "trying to add shipdata"
+  #xcoord = "4.60000000000005" + rand(1000).to_s
   #url = "http://marinetraffic.com/ais/getjson.aspx?"+
   #      "sw_x=3.8&sw_y=51.8&ne_x="+xcoord+
   #      "&ne_y=52&"+
   #      "zoom=12&fleet=&station=0"
-  url = "http://marinetraffic.com/ais/getjson.aspx?sw_x=3&sw_y=51&ne_x=5&ne_y=53&zoom=10&fleet=&station=0"
-  resp = Net::HTTP.get_response(URI.parse(url))
-  raw = resp.body
-  #clean = raw.sub(",,",",0,")
+    url = "http://marinetraffic.com/ais/getjson.aspx?sw_x=3&sw_y=51&ne_x=5&ne_y=53&zoom=10&fleet=&station=0"
+    resp = Net::HTTP.get_response(URI.parse(url))
+    raw = resp.body
+    clean = raw.gsub(",,",",0,")
+    parsed = JSON.parse(clean)
+    ship = ShipData.create(:json => parsed )
+    return parsed
+  else
+    "got content:"+ShipData.first().json
+  end
   #clean = raw.gsub(/,\s*,/ , ",0,")
 #parsed = JSON.parse(clean);
+end
+
+get '/count' do
+  "count:"+ShipData.count.to_s
 end
